71549e2477305042238184ec16c6f27acda5c51d
saivihari18 <saivihari.m@techouts.com>
2025-06-16 18:19:38 +0530

    Code Change

--- WordSum.java
+++ WordSum.java
@@ -1,6 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class WordSum {
 
     public static void main(String[] args) {
 
+        String[] str = {"Apple", "Banana", "Papaya", "Grapes"};
+
+        List<String> list = Arrays.asList(str);
+
+        int sum = list.stream().mapToInt(m -> m.charAt(0)).sum();
+
+        System.out.println("Sum of first index is " + sum);
     }
 }
